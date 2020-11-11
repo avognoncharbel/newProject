300c8231da87b8b7ad8f71f1558c6b312588be92
Charbel AVOGNON <avognoncharbel@gmail.com>
2020-11-11 13:20:58 -0600

    Initial commit

---
+++ src/Practice.java
@@ -0,0 +1,13 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		long x=0;
+		int y=1;
+		
+		for (  y=0, x=4; x<10 && y<10; x++,y++) {
+			System.out.println(y + " "+x);
+		}
+	}
+
+}
